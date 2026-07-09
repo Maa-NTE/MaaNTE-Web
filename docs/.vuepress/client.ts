a4452347de85be5711d812d9b8e9dea114a01fc9
渡辺鳩P <42133452+Watanabehato@users.noreply.github.com>
2026-07-09 16:37:44 +0800

    fix: remove duplicated root locale from switcher (#74)

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,4 +1,5 @@
 import { defineClientConfig } from 'vuepress/client'
+import { themeData } from 'vuepress-theme-plume/client'
 import Layout from './layouts/Layout.vue'
 import QQGroupJoin from './components/QQGroupJoin.vue'
 import Redirect from './components/Redirect.vue'
@@ -6,12 +7,22 @@ import './styles/custom.css'
 import HomeIntro from './components/HomeIntro.vue'
 import HHero from './components/HHero.vue'
 
+function removeRootLocaleFromSwitcher() {
+  const locales = themeData.value.locales
+  if (locales?.['/'] && locales['/zh_cn/']) {
+    delete locales['/']
+  }
+}
+
+removeRootLocaleFromSwitcher()
+
 export default defineClientConfig({
   layouts: {
     Layout,
   },
 
   enhance({ app }) {
+    removeRootLocaleFromSwitcher()
     app.component('HomeIntro', HomeIntro)
     app.component('HHero', HHero)
     app.component('Redirect', Redirect)
